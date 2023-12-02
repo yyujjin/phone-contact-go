9f50d710f616e60fcdfbbd2b39bb948f301dea2b
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-03 08:28:24 +0900

    main.go에서 유저 배열을 가져오는 라우터를 생성하고 js랑 연결함

--- assets/js/contact-list.js
+++ assets/js/contact-list.js
@@ -1,19 +1,37 @@
 const addButton = document.querySelector("#addButton")
 
-const users = [
-    {
-        name : "박유진",
-        phone : "010-9392-7723"
-    },
-    {
-        name : "박수현",
-        phone : "010-9392-7723"
+getUsers()
+
+
+// let users = null  //null 이라 해도 됨? 아니면 안 적어도 됨 ? 자바니까?
+//코드 읽는 순서
+//user-management .js 에는 
+// let users = []이렇게 했는데 굳이 뺀 이유가 있는지 그리고 
+//a [] = b[] 배열에 배열이 들어오면 그냥 배열이 되는건지
+//let 은 const쓰면 안되는지 
+async function getUsers() {
+    let users = null
+    const res = await fetch("http://localhost:8080/getUsers")
+    const data = await res.json()
+    users = data
+    console.log(users)
+    makeList(users)
+}
+    
+function makeList(users) {
+    const form = document.querySelector("form")
+    form.innerHTML=""
+    for (let i=0; i<users.length; i++) {
+        form.innerHTML +=
+            `<div>
+                <span>${users[i].Name}</span>
+                <span>${users[i].Number}</span>
+                <button class="deleteButtons"></button>
+                <button class="editButtons" ></button>
+            </div>` 
     }
-]
-
-makeList()
-
-
+    deleteUser()
+}
 
 function deleteUser() {
     const deleteButtons = document.querySelectorAll(".deleteButtons")
@@ -25,20 +43,3 @@ function deleteUser() {
         })
     }
 }
-
-function makeList() {
-    const form = document.querySelector("form")
-    form.innerHTML=""
-    for (let i=0; i<users.length; i++) {
-        form.innerHTML +=
-            `<div>
-                <span>${users[i].name}</span>
-                <span>${users[i].phone}</span>
-                <button class="deleteButtons"></button>
-                <button class="editButtons" ></button>
-            </div>` 
-    }
-    deleteUser()
-}
-   
-    
--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
+
+
 
 func main() {
   r := gin.Default() //이건 뭐야?
   r.Static("/assets", "./assets")  //여기 main에 있는 코드들 그냥 복붙하면 되는거? 아님 외워야하는거? 코드를?
 	r.LoadHTMLGlob("templates/*")
+
+  //이거 대문자 써야 먹히는 이유 
+  type user struct{
+    Name string
+    Number int
+  }
+  
+  //010붙이니까 안돼. 무슨 진수 뭐시기 하는데 뭔말
+  users := []user{
+    {"박유진",93927723},
+    {"박수현",85566254},
+  }
 
 
   r.GET("/list", func(c *gin.Context) {
@@ -22,6 +37,19 @@ func main() {
   r.GET("/edit", func(c *gin.Context) {
     c.HTML(http.StatusOK,"edit-contact.html", gin.H{})
   })
+
+  r.GET("/getUsers", func(c *gin.Context) {
+    fmt.Println(users)
+    c.JSON(200,users)
+  })
+
+
+
+// fmt.Println(users)
+
+
+
+
 
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
