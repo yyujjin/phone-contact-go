5aae599db1c93bbc7c40359ed577871bb89cca53
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-03 06:08:48 +0900

    GO 에서 GET 라우트를 구현하고 HTML 연결함

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
   "net/http"
-
   "github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
+  r := gin.Default() //이건 뭐야?
+  r.Static("/assets", "./assets")  //여기 main에 있는 코드들 그냥 복붙하면 되는거? 아님 외워야하는거? 코드를?
+	r.LoadHTMLGlob("templates/*")
+
+
+  r.GET("/list", func(c *gin.Context) {
+    c.HTML(http.StatusOK,"contact-list.html", gin.H{})
   })
+
+  r.GET("/add", func(c *gin.Context) {
+    c.HTML(http.StatusOK,"add-contact.html", gin.H{})
+  })
+
+  r.GET("/edit", func(c *gin.Context) {
+    c.HTML(http.StatusOK,"edit-contact.html", gin.H{})
+  })
+
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
