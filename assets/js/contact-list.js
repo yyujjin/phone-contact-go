0775c22fc87deecdc735269253f0dc41fea46f7a
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-03 09:44:51 +0900

    휴지통 버튼 클릭시 삭제 라우터 동작 구현

--- assets/js/contact-list.js
+++ assets/js/contact-list.js
@@ -1,17 +1,16 @@
 const addButton = document.querySelector("#addButton")
-addButton.addEventListener("click", function(){
+addButton.addEventListener("click", function () {
     location.href = "http://localhost:8080/add"
 })
 
 getUsers()
 
-
 // let users = null  //null 이라 해도 됨? 아니면 안 적어도 됨 ? 자바니까?
 //코드 읽는 순서
-//user-management .js 에는 
-// let users = []이렇게 했는데 굳이 뺀 이유가 있는지 그리고 
+//user-management .js 에는
+// let users = []이렇게 했는데 굳이 뺀 이유가 있는지 그리고
 //a [] = b[] 배열에 배열이 들어오면 그냥 배열이 되는건지
-//let 은 const쓰면 안되는지 
+//let 은 const쓰면 안되는지
 async function getUsers() {
     let users = null
     const res = await fetch("http://localhost:8080/getUsers")
@@ -20,29 +19,37 @@ async function getUsers() {
     console.log(users)
     makeList(users)
 }
-    
+
 function makeList(users) {
     const form = document.querySelector("form")
-    form.innerHTML=""
-    for (let i=0; i<users.length; i++) {
-        form.innerHTML +=
-            `<div>
+    form.innerHTML = ""
+    for (let i = 0; i < users.length; i++) {
+        form.innerHTML += `<div>
                 <span>${users[i].Name}</span>
                 <span>${users[i].Number}</span>
                 <button class="deleteButtons"></button>
                 <button class="editButtons" ></button>
-            </div>` 
+            </div>`
     }
-    deleteUser()
+    const deleteButtons = document.querySelectorAll(".deleteButtons")
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function () {
+            deleteUser(i)
+        })
+    }
 }
 
-function deleteUser() {
-    const deleteButtons = document.querySelectorAll(".deleteButtons")
-    for (let i=0; i<deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener("click",function() {
-            alert("삭제하시겠습니까?")
-            users.splice(i,1)
-            makeList()
+async function deleteUser(i) {
+    const confirmDelete = confirm("삭제하시겠습니까?")
+    if (!confirmDelete) {
+        return
+    }
+    try {
+        await fetch(`http://localhost:8080/delete/${i}`, {
+            method: "DELETE",
         })
+        getUsers()
+    } catch (error) {
+        console.error("네트워크 오류:", error)
     }
 }
