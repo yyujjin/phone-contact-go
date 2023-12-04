2b79ccffd828fd8d4b863a81b2ab376c95ac0f4c
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-04 23:32:36 +0900

    불필요한 파일삭제

--- assets/js/add.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelectorAll("inpur")
-const cancleButton = document.querySelector("#cancleButton")
-cancleButton.addEventListener("clilck",function(){
-    requ="false"
-    input[1].ariaRequired="false"
-})
